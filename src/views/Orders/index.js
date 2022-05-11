0ef56a066fb9ade6b8ebbdac4f0290718183498e
Xatai123 <xataialiyev5@gmail.com>
2022-05-11 16:16:40 +0400

    hmm

--- src/views/Orders/index.js
+++ src/views/Orders/index.js
@@ -23,20 +23,14 @@ const Order = () => {
   password.current = watch("password", "")
   const onSubmit = (data) => {
     const { menus } = data
-    console.log(data)
-
-    // const { username, password } = data
-    // let formData = new FormData()
-    // formData.set("username", username)
-    // formData.set("password", password)
-
-    // API.register(formData)
-    //     .then(() => {
-    //         navigate("/login", { replace: true })
-    //     })
-    //     .catch((e) => {
-    //         console.log(e)
-    //     })
+    let res  = []
+    for (let i = 0; i < menuItems.length; i++) {
+      menus[i].menu_id = menuItems[i].id;
+      if(menus[i].quantity){
+        res.push(menus[i])
+      }
+    }
+    console.log(res)
   }
 
   return (
@@ -44,14 +38,17 @@ const Order = () => {
       <h1 className="mt-20 mb-9 text-center text-3xl">Order</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="grid gap-5 auth">
         {menuItems.map((item, i) => (
-          <div key={i} className="grid grid-cols-2">
+          <div key={i} className="grid grid-cols-2 relative">
+            <img src={item.image} className="w-[10em] h-[14em]" alt="menu" />
+            <div>
             <p>{item.mealName}</p>
             <input
               type="number"
               placeholder="0"
               className="dark:text-black"
               {...register("menus." + i + ".quantity", { required: false })}
-            />
+              />
+              </div>
           </div>
         ))}
 
